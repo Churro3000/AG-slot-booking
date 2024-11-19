deec5d667230f6bfdaf1f6535c63d1261539afbc
Churro3000 <churro25200@gmail.com>
2024-11-19 15:08:43 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,3 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyAZ2BEsDvVRv9GIoNyA1wkIW4FqpTTcuxA",
-    authDomain: "archer-slot-booking.firebaseapp.com",
-    projectId: "archer-slot-booking",
-    storageBucket: "archer-slot-booking.appspot.com",
-    messagingSenderId: "96539149968",
-    appId: "1:96539149968:web:23d9987e0a0cf2e1137a15"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
 // Bow and arrow emoji button
 const emojiBtn = document.getElementById('emoji-btn');
 const bookingTableContainer = document.getElementById('booking-table-container');
