38b2f7c59995ae88ac295b9f2df9f2047c0d90c3
Churro3000 <churro25200@gmail.com>
2024-11-21 12:50:05 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,23 @@
-import { app, db } from './fbc.js';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAZ2BEsDvVRv9GIoNyA1wkIW4FqpTTcuxA",
+    authDomain: "archer-slot-booking.firebaseapp.com",
+    projectId: "archer-slot-booking",
+    storageBucket: "archer-slot-booking.appspot.com",
+    messagingSenderId: "96539149968",
+    appId: "1:96539149968:web:23d9987e0a0cf2e1137a15"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+// Export the Firebase app and Firestore instance
+export { app, db };
 
 // Bow and arrow emoji button
 const emojiBtn = document.getElementById('emoji-btn');
